ad5fbc2ab05fc9c86d351478683e9c32fbe6d89a
eyjiang <eyjiang@umich.edu>
2020-05-10 06:10:53 -0400

    Comment and linted (autopep8) code

--- server/app.py
+++ server/app.py
@@ -6,6 +6,7 @@ import config
 
 # configuration
 DEBUG = True
+API_KEY = config.api_key
 
 # instantiate the app
 app = Flask(__name__)
@@ -14,10 +15,8 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-
-API_KEY = config.api_key
-
-
+# Route to retrieve Flickr photos based on user_input search
+# Uses flickr.photos.search endpoint
 @app.route('/photos', methods=['GET'])
 def retrieve_photos():
     search_text = request.args['user_input']
@@ -33,11 +32,16 @@ def retrieve_flickr_imgs(search_text):
     photo_url_list = []
 
     try:
-        r = requests.get('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key={API_KEY}&text={search_text}&per_page=20&page=1&format=json&nojsoncallback=1'.format(API_KEY=API_KEY, search_text=search_text)).json()
+        r = requests.get(
+            'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key={API_KEY}&text={search_text}&per_page=20&page=1&format=json&nojsoncallback=1'.format(
+                API_KEY=API_KEY,
+                search_text=search_text)).json()
     # Currently handles failed requests as simply returning "No Results"
     except requests.exceptions.RequestException as e:
         return []
 
+    # Constructs a list of photo urls using info retrieved from GET request
+    # as described by: https://www.flickr.com/services/api/misc.urls.html
     photos = r['photos']['photo']
     for photo in photos:
         photo_id = photo['id']
@@ -45,7 +49,8 @@ def retrieve_flickr_imgs(search_text):
         server_id = photo['server']
         secret = photo['secret']
 
-        photo_url = 'https://farm{farm}.staticflickr.com/{server}/{id}_{secret}.jpg'.format(farm=farm_id, server=server_id, id=photo_id, secret=secret)
+        photo_url = 'https://farm{farm}.staticflickr.com/{server}/{id}_{secret}.jpg'.format(
+            farm=farm_id, server=server_id, id=photo_id, secret=secret)
 
         photo_url_list.append(photo_url)
 
